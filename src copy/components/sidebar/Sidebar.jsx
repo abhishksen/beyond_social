a7a08c8bf85316854832729692a318f8e91b1be3
Abhishek <codmonk21@gmail.com>
2023-12-05 23:51:50 +0530

    commit changes

---
+++ src copy/components/sidebar/Sidebar.jsx
@@ -0,0 +1,119 @@
+import { Avatar, Box, Flex, Link, Text, Tooltip } from "@chakra-ui/react"
+import { Link as RouterLink } from "react-router-dom"
+import { CreatePostLogo, Logo, LogoMobile, NotificationsLogo, SearchLogo } from "../../assets/constant"
+import { AiFillHome } from "react-icons/ai"
+import { BiLogOut } from "react-icons/bi"
+
+const Sidebar = () => {
+
+    const sidebarItems = [
+        {
+            icon: <AiFillHome size={25} />,
+            text: 'Home',
+            link: '/'
+        },
+        {
+            icon: <SearchLogo />,
+            text: 'Search',
+        },
+        {
+            icon: <NotificationsLogo />,
+            text: 'Notifications',
+        },
+        {
+            icon: <CreatePostLogo />,
+            text: 'Create',
+        },
+        {
+            icon: <Avatar size={'sm'} name="Abhishek Sen" src="/profilepic.jpg" />,
+            text: 'Profile',
+            link: '/profile'
+        }
+    ]
+
+    return (
+        <Box
+            height={'100vh'}
+            borderRight={'1px solid'}
+            borderColor={'whiteAlpha.300'}
+            py={8}
+            position={'sticky'}
+            top={0}
+            left={0}
+            px={{ base: 2, md: 4 }}
+        >
+            <Flex
+                direction={'column'}
+                gap={10}
+                w={'full'}
+                h={'full'}
+            >
+                <Link to={'/'} as={RouterLink} display={{ base: 'none', md: 'block' }} cursor={'pointer'}>
+                    <Logo />
+                </Link>
+                <Link to={'/'} as={RouterLink} display={{ base: 'block', md: 'none' }} cursor={'pointer'}>
+                    <LogoMobile />
+                </Link>
+                <Flex direction={'column'} gap={5} cursor={'pointer'}
+                    justifyContent={{ base: 'center', md: 'flex-start' }}
+                    alignItems={{ base: 'center', md: 'flex-start' }}
+                >
+                    {
+                        sidebarItems.map((item, index) => (
+                            <Tooltip
+                                hasArrow
+                                label={item.text}
+                                placement={'right'}
+                                key={index}
+                                ml={1}
+                                openDelay={500}
+                                display={{ base: 'block', md: 'none' }}
+                            >
+                                <Link as={RouterLink}
+                                    to={item.link || null}
+                                    display={'flex'}
+                                    gap={5}
+                                    p={2}
+                                    w={{ base: '10', md: 'full' }}
+                                    justifyContent={{ base: 'center', md: 'flex-start' }}
+                                    alignItems={{ base: 'center', md: 'flex-start' }}
+                                    borderRadius={6}
+                                    _hover={{ bg: 'whiteAlpha.400' }}
+                                >
+                                    {item.icon}
+                                    <Text display={{ base: 'none', md: 'block' }}>{item.text}</Text>
+                                </Link>
+                            </Tooltip>
+                        ))
+                    }
+                </Flex>
+                <Tooltip
+                    hasArrow
+                    label={'Logout'}
+                    placement={'right'}
+                    ml={1}
+                    openDelay={500}
+                    display={{ base: 'block', md: 'none' }}
+                >
+                    <Link as={RouterLink}
+                        to={'/auth'}
+                        display={'flex'}
+                        gap={5}
+                        p={2}
+                        w={{ base: '10', md: 'full' }}
+                        justifyContent={{ base: 'center', md: 'flex-start' }}
+                        alignItems={{ base: 'center', md: 'flex-start' }}
+                        borderRadius={6}
+                        _hover={{ bg: 'whiteAlpha.400' }}
+                        mt={'auto'}
+                    >
+                        <BiLogOut size={25} />
+                        <Text display={{ base: 'none', md: 'block' }}>Logout</Text>
+                    </Link>
+                </Tooltip>
+            </Flex>
+        </Box>
+    )
+}
+
+export default Sidebar
