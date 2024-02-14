b65ab0b729de16bb8f5e3965466de2c1b6a56fba
Abhishek <codmonk21@gmail.com>
2024-02-14 13:48:08 +0530

    added delete post functionality

--- src/components/profile/ProfilePost.jsx
+++ src/components/profile/ProfilePost.jsx
@@ -1,14 +1,47 @@
-import { Avatar, Box, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react"
+import { Avatar, Button, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react"
 import { AiFillHeart } from "react-icons/ai"
 import { FaComment } from "react-icons/fa"
 import { MdDelete } from "react-icons/md"
 import Comment from "../comment/Comment"
 import PostFooter from "../feedpost/PostFooter"
 import useUserProfileStore from "../../store/userProfileStore"
+import useAuthStore from "../../store/authStore"
+import useShowToast from "../../hooks/useShowToast"
+import { useState } from "react"
+import { deleteObject, ref } from "firebase/storage"
+import { firestore, storage } from "../../firebase/firebase"
+import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore"
+import usePostStore from "../../store/postStore"
 
 const ProfilePost = ({ post }) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const userProfile = useUserProfileStore((state) => state.userProfile);
+    const authUser = useAuthStore((state) => state.user);
+    const showToast = useShowToast();
+    const [isDeleting, setIsDeleting] = useState(false);
+    const deletePost = usePostStore((state) => state.deletePost);
+
+    const handleDeletePost = async () => {
+        if (!window.confirm("Are you sure you want to delete this post?")) return;
+        if (isDeleting) return;
+        try {
+            setIsDeleting(true);
+            const imageRef = ref(storage, `posts/${post.id}`);
+            await deleteObject(imageRef);
+            const userRef = doc(firestore, "users", authUser.uid);
+            await deleteDoc(doc(firestore, "posts", post.id));
+            await updateDoc(userRef, {
+                posts: arrayRemove(post.id)
+            });
+            deletePost(post.id);
+            showToast("Success", "Post deleted successfully", "success");
+        } catch (error) {
+            showToast("Error", error.message, "error");
+        } finally {
+            setIsDeleting(false);
+        }
+    }
+
     return (
         <>
             <GridItem
@@ -112,9 +145,20 @@ const ProfilePost = ({ post }) => {
                                             {userProfile.userName}
                                         </Text>
                                     </Flex>
-                                    <Box _hover={{ bg: "whiteAlpha.300", color: "red.600" }} borderRadius={4} p={1}>
-                                        <MdDelete size={20} cursor="pointer" />
-                                    </Box>
+                                    {
+                                        authUser.uid === userProfile.uid && (
+                                            <Button
+                                                size={"sm"}
+                                                bg={"transparent"}
+                                                _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                                                borderRadius={4} p={1}
+                                                onClick={handleDeletePost}
+                                                isLoading={isDeleting}
+                                            >
+                                                <MdDelete size={20} cursor="pointer" />
+                                            </Button>
+                                        )
+                                    }
                                 </Flex>
 
                                 <Divider my={5} bg={"gray.700"} />
--- src/store/postStore.js
+++ src/store/postStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 const usePostStore = create((set) => ({
     posts: [],
     createPost: (post) => set(state => ({ posts: [post, ...state.posts] })),
-    // deletePost: (id) => set(state => ({ posts: state.posts.filter(post => post.id !== id) })),
+    deletePost: (id) => set(state => ({ posts: state.posts.filter(post => post.id !== id) })),
     // addComment: (id, comment) => set(state => ({ posts: state.posts.map(p => p.id === id ? { ...p, comments: [...p.comments, comment] } : p) })),
     setPosts: (posts) => set({ posts }),
 }));
