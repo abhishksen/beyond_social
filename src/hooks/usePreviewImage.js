299e3c4787ba75e7322568355c79b10c75a5aa8a
Abhishek <codmonk21@gmail.com>
2024-02-22 10:46:05 +0530

    final touches ig

--- src/hooks/usePreviewImage.js
+++ src/hooks/usePreviewImage.js
@@ -1,17 +1,17 @@
 import { useState } from "react"
-// import useShowtoast from "./useShowtoast"
+import useShowtoast from "./useShowtoast"
 
 const usePreviewImage = () => {
     const [selectedFile, setSelectedFile] = useState(null);
-    // const showToast = useShowtoast();
+    const showToast = useShowtoast();
     const maxFileSizeInBytes = 2 * 1024 * 1024; // 2MB
 
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         if (file && file.type.startsWith("image/")) {
             if (file.size > maxFileSizeInBytes) {
-                alert("File size should be less than 2MB");
-                // showToast("error", "File size should be less than 2MB");
+                // alert("File size should be less than 2MB");
+                showToast("error", "File size should be less than 2MB");
                 setSelectedFile(null);
                 return;
             }
@@ -21,8 +21,8 @@ const usePreviewImage = () => {
             }
             reader.readAsDataURL(file);
         } else {
-            // showToast("error", "File size should be less than 2MB");
-            alert("File size should be less than 2MB");
+            showToast("error", "File size should be less than 2MB");
+            // alert("File size should be less than 2MB");
             setSelectedFile(null);
 
         }
